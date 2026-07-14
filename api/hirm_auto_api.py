9ee6f66eec30cf089f0d0eb6dd10aed674e6fb86
D1anJun <wdianjunk@163.com>
2026-07-14 17:07:12 +0800

    准备集成到jenkins上生成测试报告

--- api/hirm_auto_api.py
+++ api/hirm_auto_api.py
@@ -2,14 +2,21 @@ import requests
 class HirmLoginApi:
   @classmethod
   def hirm_login(cls,json_data):
-    resp = requests.post("http://ihrm-java.itheima.net/api/sys/login",
-                         headers={"User-Agent": "PostmanRuntime/7.39.0"},
-                         json=json_data)
+    resp = requests.post("https://ihrm-java.itheima.net/api/sys/login",
+                         headers={
+                           "User-Agent": "Mozilla/5.0",
+                           "Accept": "application/json, text/plain, */*",
+                           "Content-Type": "application/json;charset=UTF-8",
+                           "Origin": "https://ihrm-java.itheima.net",
+                           "Referer": "https://ihrm-java.itheima.net/"
+                         },
+                         json=json_data,
+                         timeout=10)
     return resp
 
 
 
 if __name__ == '__main__':
-  json_data = {"mobile": "13800000002", "password": "929itheima.CN032@.20260706"}
+  json_data = {"mobile": "13800000002", "password": "929itheima.CN032@.20260714"}
   resp = HirmLoginApi.hirm_login(json_data)
   print(resp.json())
